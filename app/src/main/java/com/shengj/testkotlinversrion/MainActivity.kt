912c2989132b10c884f2ff3f48d58485209c3c5e
shengj <shengj@rd.netease.com>
2021-07-20 11:30:13 +0800

    first commit

---
+++ app/src/main/java/com/shengj/testkotlinversrion/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.shengj.testkotlinversrion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv_main.text = "hello kotlin!"
+    }
+}
